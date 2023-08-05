5890113b5d3a97936c26110b20048247324c4a2f
rishi monohur <rishi.monohur15@gmail.com>
2023-08-05 16:40:59 +0400

    6 - As a user, I can edit a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -32,6 +32,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
