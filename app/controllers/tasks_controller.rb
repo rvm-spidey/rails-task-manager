c094583621e66d3c0d9f477817bb871ccf025fb9
rishi monohur <rishi.monohur15@gmail.com>
2023-08-05 16:30:54 +0400

    6- title and details As a user, I can edit a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,6 +18,17 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    # No need for app/views/restaurants/update.html.erb
+    redirect_to task_path(@task)
+  end
+
   private
 
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
   post "tasks", to: "tasks#create"
   get "tasks", to:"tasks#index"
-  get "tasks/new", to: "tasks#new"
+  get "tasks/new", to: "tasks#new", as: :new_task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
   get "tasks/:id", to:"tasks#show", as: :task
+  patch "tasks/:id", to: "tasks#update"
 end
