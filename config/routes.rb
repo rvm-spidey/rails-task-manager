c84cc6bd7ac0a3c1de3c4cd6978fe68e8cf69997
rishi monohur <rishi.monohur15@gmail.com>
2023-08-05 15:23:17 +0400

    4 - As a user, I can view the details of a task

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "tasks", to:"tasks#index"
+  get "tasks/:id", to:"tasks#show", as: :task
 end
