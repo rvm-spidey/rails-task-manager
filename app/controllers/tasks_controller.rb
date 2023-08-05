c24f4bcd31967e5067eed35a5e626a3801cb6561
rishi monohur <rishi.monohur15@gmail.com>
2023-08-05 16:08:56 +0400

    5 - As a user, I can add a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,21 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new(params[:task])
+  end
+
+  def create
+    # raise
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post "tasks", to: "tasks#create"
   get "tasks", to:"tasks#index"
+  get "tasks/new", to: "tasks#new"
   get "tasks/:id", to:"tasks#show", as: :task
 end
