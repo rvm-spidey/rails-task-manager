09f64c966922accd74b3a60af81e6360e0274ec0
rishi monohur <rishi.monohur15@gmail.com>
2023-08-05 16:47:14 +0400

    7 - As a user, I can remove a task

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
   get "tasks/:id", to:"tasks#show", as: :task
   patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
